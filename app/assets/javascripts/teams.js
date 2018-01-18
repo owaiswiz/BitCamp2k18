11396e574de3182a73b54c7feff1e1b3fa6c9e21
Owais <owaiswiz@gmail.com>
2018-01-19 01:33:27 +0530

    Fix custom file chooser to handle click events correctly

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -2,6 +2,15 @@
 
 function handleRegistrationForm() { // eslint-disable-line no-unused-vars 
 
+  var clicked = false;
+  $('.file-upload').on('click',function() {
+    if(!clicked) {
+      clicked = true; 
+      $(this).find('input').click();
+    }
+    clicked = false;
+  });
+
   $('.chooseFile').bind('change', function () {
     var filename = $(this).val();
     var $fileUpload = $(this).closest('.file-upload');
