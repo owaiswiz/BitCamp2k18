06b0803905844ce2b314f6f8bb557deb0e3f316f
Owais <owaiswiz@gmail.com>
2018-01-19 00:39:53 +0530

    Enable accessor for required simpleform fields

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,6 @@ class Team < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  attr_accessor :team_name, :leader_name, :leader_phone, :member_1_name, :member_1_phone, :member_2_name, :member_2_phone, :member_3_name, :member_3_phone
+
 end
