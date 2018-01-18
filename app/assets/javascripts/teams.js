da7ef0ba25b52e76efe63df7a172178f30737967
Owais <owaiswiz@gmail.com>
2018-01-19 00:41:24 +0530

    Add form for registration of team

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,2 +1,19 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+/* global $ */
+
+function handleRegistrationForm() { // eslint-disable-line no-unused-vars 
+
+  $('.chooseFile').bind('change', function () {
+    var filename = $(this).val();
+    var $fileUpload = $(this).closest('.file-upload');
+    var $fileSelectName = $fileUpload.find(".file-select-name");
+    if (/^\s*$/.test(filename)) {
+      $fileSelectName.removeClass('active');
+      $fileSelectName.text("No file chosen..."); 
+    }
+    else {
+      $fileUpload.addClass('active');
+      $fileSelectName.text(filename.replace("C:\\fakepath\\", "")); 
+    }
+  });
+
+}
