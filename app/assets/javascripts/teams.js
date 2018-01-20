48530cd480fd7482e60a206cff077cc05fca65f2
Owais <owaiswiz@gmail.com>
2018-01-21 00:59:43 +0530

    Style validations and add error messages

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -2,6 +2,15 @@
 
 function handleRegistrationForm() { // eslint-disable-line no-unused-vars 
 
+  var EMPTY_INPUT = "This field is required";
+  var EMPTY_FILE = "This file is required";
+  var INVALID_TELEPHONE = "Please enter a valid phone number";
+  var INVALID_EMAIL = "Please enter a valid e-mail";
+  var INVALID_PASSWORD = "Please enter a password with atleast 6 characters";
+  var DIFFERENT_PASSWORD_CONFIRMATION = "Password and Password Confirmation do not match";
+
+  var hasErrors = false;
+
   function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email.toLowerCase());
@@ -12,13 +21,36 @@ function handleRegistrationForm() { // eslint-disable-line no-unused-vars
     var $passwordInput = $(passwordInputs[0]);
     var $passwordConfirmationInput = $(passwordInputs[1]);
     if($passwordInput.val().length < 6) {
-      console.log('password can not be less than 6 chars');
+      addError($passwordInput,INVALID_PASSWORD);
     }
     if($passwordInput.val() != $passwordConfirmationInput.val()) {
-      console.log('password and password confirmation does not match');
+      addError($passwordConfirmationInput,DIFFERENT_PASSWORD_CONFIRMATION);
     }
   }
 
+  function addError($element, error) {
+    hasErrors = true;
+    removeErrorBlock($element)
+    $element.addClass('error');
+    var errorBlockHTML = '<span class="help-block">' + error + '</span>';
+    $element.after(errorBlockHTML);
+  }
+
+  function removeErrorBlock($element) {
+    $element.removeClass('error');
+    $element.closest('div').find('.help-block').remove();
+  }
+
+  function addFileErrorBlock($element, error) {
+    removeFileErrorBlock($element);
+    addError($element, error);
+    $element.find('input').addClass('error');
+
+  }
+  function removeFileErrorBlock($element) {
+    $element.removeClass('error');
+    $element.closest('div.file-upload').find('.help-block').remove();
+  }
   var clicked = false;
   $('.file-upload').on('click',function() {
     if(!clicked) {
@@ -31,15 +63,17 @@ function handleRegistrationForm() { // eslint-disable-line no-unused-vars
   $('.chooseFile').bind('change', function () {
     var filename = $(this).val();
     var $fileUpload = $(this).closest('.file-upload');
+    var $fileSelect = $(this).closest('.file-select');
     var $fileSelectName = $fileUpload.find(".file-select-name");
     if (/^\s*$/.test(filename)) {
-      $fileSelectName.removeClass('active');
+      $fileUpload.removeClass('active');
       $fileSelectName.text("No file chosen..."); 
     }
     else {
       $fileUpload.addClass('active');
       $fileSelectName.text(filename.replace("C:\\fakepath\\", "")); 
     }
+    removeFileErrorBlock($fileSelect);
   });
 
   $('.add-remove-member').on('click', function() {
@@ -65,30 +99,48 @@ function handleRegistrationForm() { // eslint-disable-line no-unused-vars
       var $this = $(this);
       if($this.hasClass('string')) {
         if($this.val().length < 1) {
-          console.log('this input seems empty' + $this);
+          addError($this, EMPTY_INPUT);
         }
       }
       if($this.hasClass('tel')) {
         var phoneNo = $this.val().replace(/ /g, "");
         if(!$.isNumeric(phoneNo) || phoneNo.length < 10) {
-          console.log('this does not seem like a phone number');
+          addError($this, INVALID_TELEPHONE);
         }
       }
       if($this.hasClass('email')) {
         if(!validateEmail($this.val())) {
-          console.log("this does not seem like a valid email");
+          addError($this, INVALID_EMAIL);
         }
       }
       if($this.hasClass('file')) {
-        var $fileSelect = $this.closest('.file-upload');
-        if(!$fileSelect.hasClass('active')) {
-          console.log('this file is required');
+        var $fileUpload = $this.closest('.file-upload');
+        var $fileSelect = $this.closest('.file-select');
+        if(!$fileUpload.hasClass('active')) {
+          addFileErrorBlock($fileSelect, EMPTY_FILE);
         }
       }
     });
 
     validatePassword();
 
-    return false;
+    if(hasErrors) {
+      hasErrors = false;
+      var $firstErrorInput = $('input.error').first();
+      if($firstErrorInput.hasClass('file')) {
+        $firstErrorInput = $firstErrorInput.closest('.file-select');
+      }
+      $('html, body').animate({
+        scrollTop: ($firstErrorInput.offset().top - 150)
+      }, function() {
+        $('input.error, .file-select.error').animateCss('jello');
+      });
+      return false;
+    }
+  });
+
+  $('input').on('focus', function() {
+    var $this = $(this);
+    removeErrorBlock($this);
   });
 }
