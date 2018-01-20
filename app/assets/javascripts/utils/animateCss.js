2eed718d786876fd2e809f426181523efd82f1c8
Owais <owaiswiz@gmail.com>
2018-01-21 00:58:36 +0530

    Add animateCss helper

---
+++ app/assets/javascripts/utils/animateCss.js
@@ -0,0 +1,13 @@
+/* global $ */
+$.fn.extend({
+  animateCss: function (animationName, callback) {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    this.addClass('animated ' + animationName).one(animationEnd, function() {
+      $(this).removeClass('animated ' + animationName);
+      if (callback) {
+        callback();
+      }
+    });
+    return this;
+  }
+});
