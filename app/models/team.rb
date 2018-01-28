92535ae2308b940b9b47ccbf76b91fbc66277520
Owais <owaiswiz@gmail.com>
2018-01-28 20:31:44 +0530

    Add some server side validation to Teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,9 @@ class Team < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :members, inverse_of: :team
   accepts_nested_attributes_for :members
+
+  validates :name, :college_name, presence: true
 end
