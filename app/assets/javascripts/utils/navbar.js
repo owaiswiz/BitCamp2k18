56c00a43cd8bdee32c107f3b619e36a8c8164b98
Owais <owaiswiz@gmail.com>
2018-02-18 21:44:21 +0530

    Navigate sponsors and about properly

--- app/assets/javascripts/utils/navbar.js
+++ app/assets/javascripts/utils/navbar.js
@@ -1,12 +1,12 @@
 /* global $ */
 $(document).on('turbolinks:load', function() {
-  $('a.nav-link[href="/#sponsors"]').off('click.navbarjs').on('click.navbarjs',function(event){
+  $('a.nav-link.handlejs').off('click.navbarjs').on('click.navbarjs',function(event){
     if(window.location.pathname == '/') {
       event.preventDefault();
+      var section = $(this).data('navigate');
       $('html, body').animate({
-        scrollTop: $('section.sponsors').offset().top
+        scrollTop: $('section.' + section).offset().top
       });
     }
   });
-
 });
