406eb13659d618ef691efb9abb067911752c8f32
Owais <owaiswiz@gmail.com>
2018-03-02 16:20:19 +0530

    Generate devise for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   namespace :teams do
     get 'dashboard/index'
     post 'dashboard/add_member'
